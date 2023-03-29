b4ca744e525cc06bcbcf7212f8f6cee40c2c953e
jakvm1 <jakvbmiarka@gmail.com>
2023-03-29 16:44:31 +0200

    proby naprawienia argumentow z kosmosu (niezbyt udane ale inne rzeczy ponaprawialem za to)

--- Tuna Swarm Optimization/TSO.cs
+++ Tuna Swarm Optimization/TSO.cs
@@ -18,18 +18,15 @@ namespace Tuna_Swarm_Optimization
        private double const_a;
        private double[] upper_limit;
        private double[] lower_limit;
-       private double[] probabilities;
-       private double[] index;
 
         //rozwiazania
        private double[][] arguments;
        private double[] results;
        private double[][] temp_arguments;
-       private double[] temp_results;
 
         //najlepsze rozwiazanie
         public double[] best_arguments;
-        public double best_result = 100000;
+        public double best_result;
 
         //zmienne pomocnicze
         public int number_of_calls = 0;
@@ -51,11 +48,8 @@ namespace Tuna_Swarm_Optimization
             this.arguments = new double[numb_of_population][];
             this.results = new double[numb_of_population];
             this.temp_arguments = new double[numb_of_population][];
-            this.temp_results = new double[numb_of_population];
             best_arguments = new double[dimension];
-            probabilities = new double[numb_of_population];
-            index = new double[numb_of_population];
-            best_result = 0;
+            best_result = 1000000;
             this.f = f;
 
             for(int i=0; i<numb_of_population; i++)
@@ -127,28 +121,30 @@ namespace Tuna_Swarm_Optimization
         {
             Random random= new Random();
             double b = random.NextDouble();
-            alpha_1 = const_a + ((1 - const_a) * ((double)iteration / number_of_iterations));
-            alpha_2 = (1-const_a) - ((1-const_a)*((double)iteration / number_of_iterations));
-
-            double inside_l = Math.Cos(((number_of_iterations + (double)1 / iteration) - 1) * Math.PI);
-            l = Math.Exp(3 * inside_l);
-            double inside_beta = Math.Cos(2 * Math.PI * b);
-            beta = inside_beta* Math.Exp(b * l);
-
             double arg_for_p = (double)iteration / number_of_iterations;
-            p = Math.Pow((1 - arg_for_p), arg_for_p);
+            alpha_1 = const_a + ((1 - const_a) * arg_for_p);
+            alpha_2 = (1-const_a) - ((1-const_a) * arg_for_p);
 
-           // Console.WriteLine("a1 = " + alpha_1 + " a2 = " + alpha_2 + " beta = " + beta + " l = " + l + " p = " + p);
+
+            double ins_ins_l = 1.0 / iteration;
+            double inside_l = Math.Cos(((number_of_iterations + ins_ins_l) - 1) * Math.PI);
+            double insx3l = 3 * inside_l;
+            l = Math.Exp(insx3l);
+            double inside_beta = Math.Cos(2 * Math.PI * b);
+            beta = inside_beta* Math.Exp(b * l)/1000000000; //tu to jest tak chwilowo bo inaczej jakeis magiczne argumenty wyskakauja a tak sa chociaz w przedzialexD
+
+            p = Math.Pow((1 - arg_for_p), arg_for_p);
+            //Console.WriteLine("a1 = " + alpha_1 + " a2 = " + alpha_2 + " beta = " + beta + " l = " + l + " p = " + p + " arg = " + arg_for_p);
 
         }
         void update_best()
         {
 
            // best_result = results[0];
-            for (int j = 0; j < dimension; j++)
-            {
-                best_arguments[j] = arguments[0][j];
-            }
+            //for (int j = 0; j < dimension; j++)
+            //{
+            //    best_arguments[j] = arguments[0][j];
+            //}
             for (int i=0; i<numb_of_population; i++)
             {
                 if (results[i]<best_result)
@@ -177,13 +173,17 @@ namespace Tuna_Swarm_Optimization
             double[] new_args = new double[dimension];
             for (int j=0; j<dimension; j++)
             {
+                double diff = Math.Abs(best_arguments[j] - arguments[i][j]);
+                Console.WriteLine("ROZNICA: " + diff);
+                double multiply_by_a = best_arguments[j] + (beta * diff);
+
                 if (i == 0)
                 {
-                    new_args[j] = alpha_1* (best_arguments[j] + beta*Math.Abs(best_arguments[j] - arguments[i][j])) + alpha_2*arguments[i][j];
+                    new_args[j] = (alpha_1* multiply_by_a) + (alpha_2*arguments[i][j]);
                 }
                 else
                 {
-                    new_args[j] = alpha_1 * (best_arguments[j] + beta * Math.Abs(best_arguments[j] - arguments[i][j])) + alpha_2 * arguments[i-1][j];
+                    new_args[j] = (alpha_1 * multiply_by_a) + (alpha_2 * arguments[i-1][j]);
                 }
             }
             
@@ -198,22 +198,25 @@ namespace Tuna_Swarm_Optimization
         void equation_7_transformation(double alpha_1, double alpha_2, double beta, int i)
         {
             double[] random_point = new double[dimension];
-            for(int j=0; j<dimension; j++)
+            Random random = new Random();
+            for (int j=0; j<dimension; j++)
             {
-                Random random = new Random();
                 random_point[j] = random.NextDouble() * (upper_limit[j] - lower_limit[j]) + lower_limit[j];
             }
 
             double[] new_args = new double[dimension];
             for (int j = 0; j < dimension; j++)
             {
+                double diff = Math.Abs(random_point[j] - arguments[i][j]);
+                Console.WriteLine("ROZNICA: " + diff);
+                double multiply_by_a = random_point[j] + (beta * diff);
                 if (i == 0)
                 {
-                    new_args[j] = alpha_1 * (random_point[j] + beta * Math.Abs(random_point[j] - arguments[i][j])) + alpha_2 * arguments[i][j];
+                    new_args[j] = (alpha_1 * multiply_by_a) + (alpha_2 * arguments[i][j]);
                 }
                 else
                 {
-                    new_args[j] = alpha_1 * (best_arguments[j] + beta * Math.Abs(random_point[j] - arguments[i][j])) + alpha_2 * arguments[i - 1][j];
+                    new_args[j] = (alpha_1 * multiply_by_a) + (alpha_2 * arguments[i - 1][j]);
                 }
             }
 
@@ -236,11 +239,10 @@ namespace Tuna_Swarm_Optimization
             for (int j = 0; j < dimension; j++)
             {
                 //nie jestem pewny czy TF ma być generowane dla każdego wymiaru czy jedno dla wszystkich więc na razie ustawilem osobno dla kazdego wymiaru
-                int neg_or_pos = random.Next(1, 2);
+                int neg_or_pos = random.Next(1, 3);
                 if (neg_or_pos == 2) { neg_or_pos = -1; }
 
-                double TF = neg_or_pos * random.NextDouble();
-
+                int TF = neg_or_pos;
                 if (rand < 0.5)
                 {
                     new_args[j] = best_arguments[j] + rand * (best_arguments[j] - arguments[i][j]) + TF * p * p * (best_arguments[j] - arguments[i][j]);
@@ -273,6 +275,13 @@ namespace Tuna_Swarm_Optimization
                     Console.Write(arguments[i][j] + " ");
                 }
                 Console.Write('\n');
+            }
+
+            Console.WriteLine("BEST RESULT: " + best_result);
+            Console.Write("BEST ARGS: ");
+            for (int j = 0; j < dimension; j++)
+            {
+                Console.Write(best_arguments[j] + " ");
             }
         }
 
@@ -364,22 +373,26 @@ namespace Tuna_Swarm_Optimization
 
                     if (rand < const_z)
                     {
+                        
                         equation_1(temp_arguments, i);
                     }
 
                     else if(rand >= 0.5)
                     {
+                        
                         equation_9_transformation(rand, p, i);
                     }
 
-                    else if((double)current_iteration/number_of_iterations < rand)
+                    else if(((double)w/number_of_iterations) < rand)
                     {
                         equation_7_transformation(alpha_1, alpha_2, beta, i);
                     }
 
                     else
                     {
-                        equation_2_transformation(alpha_1, alpha_2, beta, i);
+
+                       equation_2_transformation(alpha_1, alpha_2, beta, i);
+
                     }
                 }
 
