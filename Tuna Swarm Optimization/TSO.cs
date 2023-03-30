f5501fa8f1d4f7a5d90bf05588c02e0c310d31c8
jakvbm1 <jakvbmiarka@gmail.com>
2023-03-30 22:05:08 +0200

    ok

--- Tuna Swarm Optimization/TSO.cs
+++ Tuna Swarm Optimization/TSO.cs
@@ -77,6 +77,42 @@ namespace Tuna_Swarm_Optimization
 
             SaveToFileStateOfAlghoritm();
         }
+        void Sort_population()
+        {
+            for(int i=1; i<numb_of_population; i++)
+            {
+                for (int j=0; j<numb_of_population-i; j++)
+                {
+                    if (results[j] > results[j+1])
+                    {
+                        double result_placeholder;
+                        double[] arguments_placeholder = new double[dimension];
+                        result_placeholder = results[j];
+                        for (int k=0; k<dimension; k++)
+                        {
+                            arguments_placeholder[k] = arguments[j][k];
+                        }
+<<<<<<<<< Temporary merge branch 1
+=========
+
+                        results[j] = results[j+1];
+                        results[j+1] = result_placeholder;
+
+                        for(int k=0; k<dimension; k++)
+                        {
+                            arguments[j][k] = arguments[j + 1][k];
+                            arguments[j + 1][k] = arguments_placeholder[k];
+                        }
+                    }
+                }
+            }
+
+            best_result = results[0];
+            for(int i=0; i<dimension; i++)
+            {
+                best_arguments[i] = arguments[0][i];
+            }
+        }
         void calculate_parameters(double &alpha_1, double &alpha_2,double &beta, double &l, double &p)
         {
             Random random= new Random();
@@ -200,20 +236,125 @@ namespace Tuna_Swarm_Optimization
                 {
                     new_args[i][j] = TF * p * p * arguments[i][j];
                 }
+>>>>>>>>> Temporary merge branch 2
+
+                        results[j] = results[j+1];
+                        results[j+1] = result_placeholder;
+
+                        for(int k=0; k<dimension; k++)
+                        {
+                            arguments[j][k] = arguments[j + 1][k];
+                            arguments[j + 1][k] = arguments_placeholder[k];
+                        }
+                    }
+                }
+            }
+
+            best_result = results[0];
+            for(int i=0; i<dimension; i++)
+            {
+                best_arguments[i] = arguments[0][i];
+            }
+        }
+        void calculate_parameters(double &alpha_1, double &alpha_2,double &beta, double &l, double &p)
+        {
+            Random random= new Random();
+            double b = random.NextDouble();
+            alpha_1 = const_a + (1 - cons_a) * ((double)current_iteration / number_of_iterations);
+            alpha_2 = (1-const_a) - (1-const_a)*((double)current_iteration / number_of_iterations);
+            l = Math.Exp(3 * Math.Cos((number_of_iterations + (1 / current_iteration)) + 1) * Math.PI);
+            beta = Math.Exp(b * l) * Math.Cos(2 * Math.PI * b);
+
+            double arg_for_p = current_iteration/ number_of_iterations;
+            p = Math.Pow((1 - arg_for_p), arg_for_p);
+
+        }
+        void update_best(double[] args, double res)
+        {
+            if (res < best_result)
+            {
+                best_result = res;
+                for(int i=0; i<dimension; i++)
+                {best_arguments[i] = args[i];}
+            }
+        }
+
+        void equation_1(int i)
+        {
+            Random random = new Random();
+            for(int j=0; j<dimension; j++)
+            {
+                arguments[i][j] = random.NextDouble() * (upper_limit[j] - lower_limit[j]) + lower_limit[j];
+            }
+            results[i] = f(arguments[i]);
+            number_of_calls++;
+        }
+
+
+
+        void equation_2_transformation(double alpha_1, double alpha_2, double beta, int i)
+        {
+            double[] new_args = new double[dimension];
+            for (int j=0; j<dimension; j++)
+            {
+                if (i == 0)
+                {
+                    new_args[j] = alpha_1* (best_arguments[j] + beta*Math.Abs(best_arguments[j] - arguments[i][j])) + alpha_2*arguments[i][j];
+                }
+                else
+                {
+                    new_args[j] = alpha_1 * (best_arguments[j] + beta * Math.Abs(best_arguments[j] - arguments[i][j])) + alpha_2 * arguments[i-1][j];
+                }
+            }
+            
+            for(int j=0; j<dimension; j++)
+            {
+                arguments[i][j] = new_args[j];
+            }
+            results[i] = f(arguments[i]);
+            number_of_calls++;
+            update_best(arguments[i], results[i]);
+        }
+        void equation_7_transformation(double alpha_1, double alpha_2, double beta, int i)
+        {
+            double[] random_point = new double[dimension];
+            for(int i=0; i<dimension; i++)
+            {
+                Random random = new Random();
+                random_point[i] = random.NextDouble() * (upper_limit[j] - lower_limit[j]) + lower_limit[j];
+            }
+
+            double[] new_args = new double[dimension];
+            for (int j = 0; j < dimension; j++)
+            {
+                if (i == 0)
+                {
+                    new_args[j] = alpha_1 * (random_point[j] + beta * Math.Abs(random_point[j] - arguments[i][j])) + alpha_2 * arguments[i][j];
+                }
+                else
+                {
+                    new_args[j] = alpha_1 * (best_arguments[j] + beta * Math.Abs(random_point[j] - arguments[i][j])) + alpha_2 * arguments[i - 1][j];
+                }
+            }
 
             for (int j = 0; j < dimension; j++)
             {
-                temp_arguments[i][j] = new_args[j];
+                arguments[i][j] = new_args[j];
             }
-            //results[i] = f(arguments[i]);
-            //number_of_calls++;
-            //update_best(arguments[i], results[i]);
+            results[i] = f(arguments[i]);
+            number_of_calls++;
+            update_best(arguments[i], results[i]);
         }
 
             }
-
- 
-
+            for (int j = 0; j < dimension; j++)
+            {
+                arguments[i][j] = new_args[j];
+            }
+            results[i] = f(arguments[i]);
+            number_of_calls++;
+            update_best(arguments[i], results[i]);
+        }
 
 
         public int NumberOfEvaluationFitnessFunction => throw new NotImplementedException();
